155ec48ff475003a6722c90eb3b3baedc492b544
billykagwe <billmwas2@gmail.com>
2021-08-11 13:46:23 +0300

    added onboarding email

--- pages/api/register.js
+++ pages/api/register.js
@@ -2,6 +2,8 @@
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { connectToDatabase } from "../../utils/database";
+import sgMail from "@sendgrid/mail";
+import { Task } from "../../utils/types";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
@@ -10,9 +12,36 @@ export default async function handler(req, res) {
 
     const body = JSON.parse(req.body);
 
-    donors
-      .insertOne(body)
-      .then(res.status(200).json)
-      .catch(res.status(400).json);
+    insertData(donors, body)
+      .chain(sendOnboardingEmail(body?.email))
+      .fork(res.json, res.json);
   }
 }
+
+const insertData = (donors, data) =>
+  Task((rej, res) =>
+    donors
+      .insertOne(data)
+      .then((x) =>
+        x?.insertedId ? res(x) : rej({ error: "Could not save data" })
+      )
+      .catch(rej)
+  );
+
+//////////////////////////////////////////////////////////////////////////
+const sendOnboardingEmail = (email) => () =>
+  Task((rej, res) => {
+    sgMail.setApiKey(process.env.SENDGRID_KEY);
+
+    const msg = {
+      to: email, // Change to your recipient
+      from: "blooddonars.toc@gmail.com", // Change to your verified sender
+      subject: "Oshwal Blood Donation Registration",
+      text: "Registration data uploaded successfully",
+      html: "<strong>Thank you </strong>",
+    };
+    sgMail
+      .send(msg)
+      .then(() => res({ Success: "Onboarding email sent" }))
+      .catch(rej);
+  });

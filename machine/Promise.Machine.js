0b69030a59a279529881e9d9d328414c629d2ebb
billykagwe <billmwas2@gmail.com>
2021-08-20 14:30:13 +0300

    Email changes

--- machine/Promise.Machine.js
+++ machine/Promise.Machine.js
@@ -87,7 +87,7 @@ export const PromiseMachine = (operation) =>
       },
       success: {
         after: {
-          1000: {
+          2000: {
             target: "idle",
             actions: assign({
               data: null,

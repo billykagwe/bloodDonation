699f0edf72d16a101824a6ccb4229e3bf196e5b1
nagib <nagib@MacBooks-MacBook-Pro.local>
2022-03-19 16:19:01 +0300

    done

--- components/HomePage.js
+++ components/HomePage.js
@@ -6,8 +6,13 @@ import Image from "next/image";
 const fetcher = (url) => fetch(url).then((r) => r.json());
 function HomePage() {
   const { data, error, isValidating } = useSWR("/api/donors", fetcher);
-  console.log({data})
+ console.log({data})
   const [bloodGroup, setBloodGroup] = useState("");
+  const groupData = data?.reduce((prev,curr) => {
+    const bloodGroup = curr.bloodGroup
+      return  prev[bloodGroup] ? {...prev,[bloodGroup]: prev[bloodGroup] += 1} : {...prev,[bloodGroup]: 1}
+  },{})
+
   return (
     <div className=' mt-1 mx-2 '>
       <div className='flex max-w-4xl mx-auto sm:space-x-4  text-gray-600 text-sm   flex-wrap justify-center'>
@@ -25,6 +30,12 @@ function HomePage() {
       </div>
 
       {!data && isValidating && <p className='text-center'>loading...</p>}
+      <div className="flex space-x-2 mt-2 mb-8 items-center justify-center">
+      {Object.keys(groupData).map(key => (
+        <p className="flex items-center"><span className="text-yellow-600 font-bold text-2xl ">{key}</span>:<span className="text-gray-800 text-2xl ml-2" >{groupData[key]}</span></p>
+      ))}
+      </div>
+      
       <div className='flex max-w-4xl mx-auto md:space-x-4   flex-wrap justify-center'>
         {data
           ?.filter((donor) =>
@@ -86,12 +97,13 @@ const Donor = ({ donor }) => {
                 </div>
               </div>
             </div> */}
-            <div className=' rounded sm:max-w-xs w-full mb-2'>
-              <div className='flex w-full p-2 space-y-1  text-blue-500  capitalize flex-col'>
-                <div className='flex items-center space-x-2 '>
+            <div className=' rounded sm:max-w-sm w-full mb-2'>
+              <div className='flex w-full p-2 space-y-2  text-blue-500  capitalize flex-col'>
+                <div className='flex flex-col space-y-2  '>
+                  <div className="flex items-center space-x-2">
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
-                    className='h-4 w-4'
+                    className='h-4 w-4 text-black'
                     fill='none'
                     viewBox='0 0 24 24'
                     stroke='currentColor'>
@@ -103,12 +115,22 @@ const Donor = ({ donor }) => {
                     />
                   </svg>
                   <p>{donor?.contactPhone}</p>
-                  <p>Whatsapp: {donor?.contactPhone}</p>
+                  </div>
+                <div className="flex items-center space-x-2">
+                <svg className="h-4 w-4 text-black" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <g>
+                        <path fill="none" d="M0 0h24v24H0z"/>
+                        <path fill-rule="nonzero" d="M7.253 18.494l.724.423A7.953 7.953 0 0 0 12 20a8 8 0 1 0-8-8c0 1.436.377 2.813 1.084 4.024l.422.724-.653 2.401 2.4-.655zM2.004 22l1.352-4.968A9.954 9.954 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.954 9.954 0 0 1-5.03-1.355L2.004 22zM8.391 7.308c.134-.01.269-.01.403-.004.054.004.108.01.162.016.159.018.334.115.393.249.298.676.588 1.357.868 2.04.062.152.025.347-.093.537a4.38 4.38 0 0 1-.263.372c-.113.145-.356.411-.356.411s-.099.118-.061.265c.014.056.06.137.102.205l.059.095c.256.427.6.86 1.02 1.268.12.116.237.235.363.346.468.413.998.75 1.57 1l.005.002c.085.037.128.057.252.11.062.026.126.049.191.066a.35.35 0 0 0 .367-.13c.724-.877.79-.934.796-.934v.002a.482.482 0 0 1 .378-.127c.06.004.121.015.177.04.531.243 1.4.622 1.4.622l.582.261c.098.047.187.158.19.265.004.067.01.175-.013.373-.032.259-.11.57-.188.733a1.155 1.155 0 0 1-.21.302 2.378 2.378 0 0 1-.33.288 3.71 3.71 0 0 1-.125.09 5.024 5.024 0 0 1-.383.22 1.99 1.99 0 0 1-.833.23c-.185.01-.37.024-.556.014-.008 0-.568-.087-.568-.087a9.448 9.448 0 0 1-3.84-2.046c-.226-.199-.435-.413-.649-.626-.89-.885-1.562-1.84-1.97-2.742A3.47 3.47 0 0 1 6.9 9.62a2.729 2.729 0 0 1 .564-1.68c.073-.094.142-.192.261-.305.127-.12.207-.184.294-.228a.961.961 0 0 1 .371-.1z"/>
+                    </g>
+                </svg>
+                <p> {donor?.whatsappPhone}</p>
+                </div>
+                 
                 </div>
                 <div className='flex items-center space-x-2'>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
-                    className='h-4 w-4'
+                    className='h-4 w-4 text-black'
                     fill='none'
                     viewBox='0 0 24 24'
                     stroke='currentColor'>
@@ -124,7 +146,7 @@ const Donor = ({ donor }) => {
               </div>
             </div>
           </div>
-          <div>
+          {/* <div>
             <p className=' text-sm mb-1 mx-1 text-gray-500  italic'>
               Donation Equipments
             </p>
@@ -133,7 +155,7 @@ const Donor = ({ donor }) => {
                 <DonationItem key={image?.tag} image={image} />
               ))}
             </div>
-          </div>
+          </div> */}
         </>
       }
     </div>
